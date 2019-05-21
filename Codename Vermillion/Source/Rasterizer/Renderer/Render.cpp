d34dc59073f76f563eba13bd1323b28380df58ab
Rolf Erik Gjeldnes <vyprolf@gmail.com>
2019-05-21 13:56:47 +0200

    line render

--- Codename Vermillion/Source/Rasterizer/Renderer/Render.cpp
+++ Codename Vermillion/Source/Rasterizer/Renderer/Render.cpp
@@ -18,3 +18,29 @@ void RolfGL::Draw()
 	frameBuffer.Clear(c, c, c, 1);
 	frameBuffer.RenderBufferToScreen();
 }
+
+void RolfGL::DrawLine(float x0, float y0, float x1, float y1)
+{
+	float deltax = x1 - x0;
+	float deltay = y1 - y0;
+
+	float deltaErr = 0.0f;
+	if (deltax != 0.0f)
+		deltaErr = fabs(deltay / deltax);
+	float error = 0.0f;
+
+	int sign = 1;
+	if (deltay < 0)
+		sign = -1;
+
+	int y = (int)y0;
+	for (float x = x0; x < x1; x += deltax) {
+		frameBuffer.PutPixel((int)x, y, 1, 1, 1);
+
+		error += deltaErr;
+		if (error >= 0.5f) {
+			y += sign * deltay;
+			error -= 1.0f;
+		}
+	}	
+}
--- Codename Vermillion/Source/Rasterizer/Renderer/Render.h
+++ Codename Vermillion/Source/Rasterizer/Renderer/Render.h
@@ -9,6 +9,7 @@ public:
 	~RolfGL();
 
 	void Draw();
+	void DrawLine(float x0, float y0, float x1, float y1);
 
 private:
 	Buffer frameBuffer;
