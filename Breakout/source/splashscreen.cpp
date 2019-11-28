fd0b55392847bdf26042a8c21c7f84452f69b94a
Rolf Erik Gjeldnes <vyprolf@gmail.com>
2019-11-28 15:19:07 +0100

    preparing menuing

---
+++ Breakout/source/splashscreen.cpp
@@ -0,0 +1,21 @@
+#include "splashscreen.h"
+
+SplashScreen::SplashScreen()
+{
+}
+
+void SplashScreen::Initialize()
+{
+}
+
+void SplashScreen::Deinitialize()
+{
+}
+
+void SplashScreen::Update(float delta)
+{
+}
+
+void SplashScreen::Render()
+{
+}
